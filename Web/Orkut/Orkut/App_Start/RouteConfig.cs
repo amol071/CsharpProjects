70033f2b97095c0a06dd9aa41d3365714774e201
Amol Vyavaharkar <amolvyavaharkar@gmail.com>
2024-05-25 23:48:01 +0530

    default page
    added login.cshtml as default

--- Web/Orkut/Orkut/App_Start/RouteConfig.cs
+++ Web/Orkut/Orkut/App_Start/RouteConfig.cs
@@ -18,10 +18,10 @@ namespace Orkut
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                    name: "Default",
-                    url: "{controller}/{action}/{id}",
-                    defaults: new { action = "Index", id = UrlParameter.Optional }
-                );  
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
+            );
         }
     }
 }
--- Web/Orkut/Orkut/Controllers/AccountController.cs
+++ Web/Orkut/Orkut/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Orkut.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,36 @@ namespace Orkut.Controllers
 {
     public class AccountController : Controller
     {
-        // GET: Account
-        public ActionResult Index()
+        private UserContext db = new UserContext();
+
+        // GET: Account/Login
+        public ActionResult Login()
         {
             return View();
+        }
+
+        // POST: Account/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = db.Users
+                    .FirstOrDefault(u => u.Username == model.Username && u.Password == model.Password);
+
+                if (user != null)
+                {
+                    // Authentication successful
+                    // Set up user session or cookie
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid username or password.");
+                }
+            }
+            return View(model);
         }
     }
 }
